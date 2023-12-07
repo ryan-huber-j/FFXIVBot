1b5c952cddeedc435151e5e14f0c32b3aa546528
= <hryan13x@gmail.com>
2023-12-07 16:02:59 -0700

    Add pagination

--- backends/lodestone.py
+++ backends/lodestone.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
+import re
 import requests
+
+
+_page_number_regex = re.compile('Page \d of (\d)')
 
 
 class LodestoneScraperException(Exception):
@@ -9,14 +13,17 @@ class LodestoneScraperException(Exception):
 
 
 class LodestoneScraperClient:
-  _base_url = None
-
   def __init__(self, base_url):
     self._base_url = base_url
 
-  def get_free_company_members(self, fc_id):
-    # https://na.finalfantasyxiv.com/lodestone/freecompany/9231394073691073564/member/
-    response = requests.get(f'{self._base_url}/lodestone/freecompany/{fc_id}/member')
+
+  def _call_lodestone(self, fc_id, page=None):
+    if page is None:
+      url = f'{self._base_url}/lodestone/freecompany/{fc_id}/member'
+    else:
+      url = f'{self._base_url}/lodestone/freecompany/{fc_id}/member?page={page}'
+
+    response = requests.get(url)
 
     if response.status_code == 404:
       raise LodestoneScraperException(f'Free Company {fc_id} could not be found', response.status_code)
@@ -27,7 +34,28 @@ class LodestoneScraperClient:
     elif response.status_code != 200:
       raise LodestoneScraperException(f'Failed to access FC {fc_id} members due to an unknown issue', response.status_code)
     
-    soup = BeautifulSoup(response.content, 'html.parser')
-    fc_member_name_tags = soup.find_all('p', class_='entry__name')
+    return response
+  
 
+  def _scrape_fc_member_names(self, soup):
+    fc_member_name_tags = soup.find_all('p', class_='entry__name')
     return [tag.string for tag in fc_member_name_tags]
+
+
+  def get_free_company_members(self, fc_id):
+    response = self._call_lodestone(fc_id)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    page_number_tag = soup.find('li', class_='btn__pager__current')
+    match = _page_number_regex.fullmatch(page_number_tag.string)
+    if match is None:
+      raise LodestoneScraperException(None, f'Unable to parse page number from following: {page_number_tag.string}')
+    num_pages = int(match.group(1))
+
+    member_names = self._scrape_fc_member_names(soup)
+    for page_num in range(2, num_pages+1):
+      response = self._call_lodestone(fc_id, page_num)
+      soup = BeautifulSoup(response.content, 'html.parser')
+      member_names += self._scrape_fc_member_names(soup)
+
+    return member_names
--- test/backends/test_lodestone.py
+++ test/backends/test_lodestone.py
@@ -6,6 +6,7 @@ from backends.lodestone import LodestoneScraperClient
 
 
 BASE_URL = 'https://some.lodestone.url.com'
+FC_ID = 'fc_id'
 
 
 class FakeResponse:
@@ -20,24 +21,181 @@ class TestLodestoneScraper(unittest.TestCase):
     self.mock_responses = {}
     requests.get = self.fake_lodestone_call
 
+
   def fake_lodestone_call(self, url):
     return self.mock_responses[url]
-  
-  def add_mock_response(self, fc_id, response):
-    self.mock_responses[f'{BASE_URL}/lodestone/freecompany/{fc_id}/member'] = response
+
+
+  def add_mock_response(self, fc_id, response, page=None):
+    if page is None:
+      key = f'{BASE_URL}/lodestone/freecompany/{fc_id}/member'
+    else:
+      key = f'{BASE_URL}/lodestone/freecompany/{fc_id}/member?page={page}'
+    self.mock_responses[key] = response
+
 
   def test_get_free_company_members_no_members(self):
-    fc_id = 'empty_response'
     body = '''
     <body>
       <ul class="btn__pager">
-	      <li class="btn__pager__current">Page 1 of 3</li>
+	      <li class="btn__pager__current">Page 1 of 1</li>
       </ul>
       <ul>
 		  </ul>
     </body>
     '''
 
-    self.add_mock_response(fc_id, FakeResponse(200, '<body></body>'))
-    results = self.scraper.get_free_company_members(fc_id)
+    self.add_mock_response(FC_ID, FakeResponse(200, body))
+    results = self.scraper.get_free_company_members(FC_ID)
     self.assertListEqual(list(results), [])
+
+
+  def test_get_free_company_members_one_member_one_page(self):
+    body = '''
+    <body>
+      <ul class="btn__pager">
+	      <li class="btn__pager__current">Page 1 of 1</li>
+      </ul>
+      <ul>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">Kiryuin Satsuki</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+		  </ul>
+    </body>
+    '''
+
+    self.add_mock_response(FC_ID, FakeResponse(200, body))
+    results = self.scraper.get_free_company_members(FC_ID)
+    self.assertListEqual(list(results), ['Kiryuin Satsuki'])
+
+
+  def test_get_free_company_members_two_members_one_page(self):
+    body = '''
+    <body>
+      <ul class="btn__pager">
+	      <li class="btn__pager__current">Page 1 of 1</li>
+      </ul>
+      <ul>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">Kiryuin Satsuki</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">Juhdu Khigbaa</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+		  </ul>
+    </body>
+    '''
+
+    self.add_mock_response(FC_ID, FakeResponse(200, body))
+    results = self.scraper.get_free_company_members(FC_ID)
+    self.assertListEqual(list(results), ['Kiryuin Satsuki', 'Juhdu Khigbaa'])
+
+
+  def test_get_free_company_members_many_members_three_pages(self):
+    page1 = '''
+    <body>
+      <ul class="btn__pager">
+	      <li class="btn__pager__current">Page 1 of 3</li>
+      </ul>
+      <ul>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">Kiryuin Satsuki</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">Juhdu Khigbaa</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+		  </ul>
+    </body>
+    '''
+
+    page2 = '''
+    <body>
+      <ul class="btn__pager">
+	      <li class="btn__pager__current">Page 1 of 3</li>
+      </ul>
+      <ul>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">FC Member 3</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">FC Member 4</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+		  </ul>
+    </body>
+    '''
+
+    page3 = '''
+    <body>
+      <ul class="btn__pager">
+	      <li class="btn__pager__current">Page 1 of 3</li>
+      </ul>
+      <ul>
+        <li class="entry">
+          <a href="href" class="entry__bg">
+            <div class="entry__flex">
+              <div class="entry__freecompany__center">
+                <p class="entry__name">FC Member 5</p>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </li>
+		  </ul>
+    </body>
+    '''
+
+    self.add_mock_response(FC_ID, FakeResponse(200, page1))
+    self.add_mock_response(FC_ID, FakeResponse(200, page2), 2)
+    self.add_mock_response(FC_ID, FakeResponse(200, page3), 3)
+
+    results = self.scraper.get_free_company_members(FC_ID)
+    self.assertListEqual(list(results), ['Kiryuin Satsuki', 'Juhdu Khigbaa', 'FC Member 3', 'FC Member 4', 'FC Member 5'])
