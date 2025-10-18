057895938753e60cefbabaaebaf8726f6d4358ba
Ryan Huber <hryan13x@gmail.com>
2025-10-17 17:05:20 -0700

    Refactor into one class per method

--- test/test_lodestone.py
+++ test/test_lodestone.py
@@ -9,20 +9,21 @@ HOSTNAME = 'some.lodestone.url.com'
 FC_ID = 'fc_id'
 WORLD_NAME = 'Siren'
 
-class TestLodestoneScraper(unittest.TestCase):
+
+class TestGetFreeCompanyMembers(unittest.TestCase):
   def setUp(self):
     self.scraper = LodestoneScraper('https://' + HOSTNAME)
 
 
   @responses.activate
-  def test_get_free_company_members_no_members(self):
+  def test_no_members(self):
     response = mock_fc_members_response(HOSTNAME, 200, FC_ID, members=[])
     responses.add(response)
     self.assertListEqual(self.scraper.get_free_company_members(FC_ID), [])
 
 
   @responses.activate
-  def test_get_free_company_members_one_member_one_page(self):
+  def test_one_member_one_page(self):
     members=[FCMember('id', 'Kiryuin Satsuki', 'Big Boss')]
     response = mock_fc_members_response(HOSTNAME, 200, FC_ID, members=members)
     responses.add(response)
@@ -30,7 +31,7 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_free_company_members_two_members_one_page(self):
+  def test_two_members_on_one_page(self):
     members=[FCMember('id', 'Kiryuin Satsuki', 'Big Boss'), FCMember('id2', 'Aia Merry', 'The Boss')]
     response = mock_fc_members_response(HOSTNAME, 200, FC_ID, members=members)
     responses.add(response)
@@ -38,7 +39,7 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_free_company_members_many_members_three_pages(self):
+  def test_many_members_over_three_pages(self):
     page1_members = [FCMember('id', 'Kiryuin Satsuki', 'Big Boss'), FCMember('id2', 'Aia Merry', 'The Boss')]
     page2_members = [FCMember('id3', 'Juhdu Khigbaa', 'Made Member'), FCMember('id4', 'Boy Detective', 'Lieutenant')]
     page3_members = [FCMember('id3', 'Cirina Qalli', 'Officer')]
@@ -55,15 +56,20 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_free_company_members_error_states(self):
+  def test_error_states(self):
     for status_code in [400, 404, 429, 500]:
       response = mock_fc_members_response(HOSTNAME, status_code, FC_ID)
       responses.add(response)
       self.assertRaises(LodestoneScraperException, lambda: self.scraper.get_free_company_members(FC_ID))
 
 
+class TestGetGranCompanyRankings(unittest.TestCase):
+  def setUp(self):
+    self.scraper = LodestoneScraper('https://' + HOSTNAME)
+
+
   @responses.activate
-  def test_get_grand_company_rankings_single(self):
+  def test_single_grand_company(self):
     rankings = [GrandCompanyRanking('id', 'Kiryuin Satsuki', 1, 22000000)]
     responses.add(mock_gc_rankings_response(HOSTNAME, 200, WORLD_NAME, rankings, 1))
     responses.add(mock_gc_rankings_response(HOSTNAME, 200, WORLD_NAME, [], 2))
@@ -74,7 +80,7 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_grand_company_rankings_two_across_two_pages(self):
+  def test_two_pages(self):
     page1_rankings = [GrandCompanyRanking('id', 'Kiryuin Satsuki', 1, 22000000)]
     page2_rankings = [GrandCompanyRanking('id2', 'Vespertine Celeano', 2, 10000000)]
     responses.add(mock_gc_rankings_response(HOSTNAME, 200, WORLD_NAME, page1_rankings, 1))
@@ -86,7 +92,7 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_grand_company_rankings_many_across_multiple_pages(self):
+  def test_multiple_rankings_across_multiple_pages(self):
     page1_rankings = [GrandCompanyRanking('id', 'Kiryuin Satsuki', 1, 22000000), GrandCompanyRanking('id2', 'Vespertine Celeano', 2, 10000000)]
     page2_rankings = [GrandCompanyRanking('id', 'GC Ranking 3', 3, 100000), GrandCompanyRanking('id4', 'GC Ranking 4', 4, 50000)]
     page3_rankings = [GrandCompanyRanking('id', 'GC Ranking 5', 5, 1000)]
@@ -99,21 +105,26 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_grand_company_rankings_error_states(self):
+  def test_error_states(self):
     for status_code in [400, 404, 429, 500]:
       responses.add(mock_gc_rankings_response(HOSTNAME, status_code, WORLD_NAME, None, 1))
       self.assertRaises(LodestoneScraperException, lambda: self.scraper.get_grand_company_rankings(WORLD_NAME))
 
 
+class TestSearchFreeCompanies(unittest.TestCase):
+  def setUp(self):
+    self.scraper = LodestoneScraper('https://' + HOSTNAME)
+
+
   @responses.activate
-  def test_search_free_companies_empty(self):
+  def test_empty(self):
     response = mock_free_companies_response(HOSTNAME, 200, WORLD_NAME)
     responses.add(response)
     self.assertEqual(self.scraper.search_free_companies(WORLD_NAME), [])
 
   
   @responses.activate
-  def test_search_free_companies_single(self):
+  def test_single(self):
     single_fc = [FreeCompany('1234', 'Free Company 1')]
     response = mock_free_companies_response(HOSTNAME, 200, WORLD_NAME, single_fc)
     responses.add(response)
@@ -121,7 +132,7 @@ class TestLodestoneScraper(unittest.TestCase):
 
 
   @responses.activate
-  def test_get_grand_company_rankings_error_states(self):
+  def test_error_states(self):
     for status_code in [400, 404, 429, 500]:
       response = mock_free_companies_response(HOSTNAME, status_code, WORLD_NAME)
       responses.add(response)
