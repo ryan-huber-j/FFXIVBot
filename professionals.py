c585868842fdb75cd8e903d605985ccb4a12c100
= <hryan13x@gmail.com>
2025-10-23 00:36:37 -0700

    Rename professionals

---
+++ professionals.py
@@ -0,0 +1,211 @@
+import random
+
+from db import SqlLiteClient
+from domain import *
+from lodestone import LodestoneScraper
+
+FREE_COMPANY_ID = "9231394073691073564"
+WORLD = "Siren"
+
+_db = None
+_lodestone = None
+
+
+def initialize(db: SqlLiteClient, scraper: LodestoneScraper):
+    global _db, _lodestone
+    _db = db
+    _lodestone = scraper
+
+
+def validate_discord_id(discord_id) -> list[ValidationError]:
+    errors = []
+    if not isinstance(discord_id, int):
+        errors.append(ValidationError("discord_id", "must be an integer."))
+    return errors
+
+
+def validate_participant(participant: Participant) -> list[ValidationError]:
+    errors = validate_discord_id(participant.discord_id)
+    if not participant.first_name.isalpha():
+        errors.append(ValidationError("first_name", "must be non-empty and alphabetic."))
+    if not participant.last_name.isalpha():
+        errors.append(ValidationError("last_name", "must be non-empty and alphabetic."))
+    return errors
+
+
+def validate_contract(
+    contract: Contract, contract_amounts: list[int]
+) -> list[ValidationError]:
+    errors = validate_discord_id(contract.discord_id)
+    if contract.amount not in contract_amounts and len(contract_amounts):
+        if len(contract_amounts) == 1:
+            contract_amounts_str = f"must be {contract_amounts[0]}."
+        elif len(contract_amounts) == 2:
+            contract_amounts_str = (
+                f"must be {contract_amounts[0]} or {contract_amounts[1]}."
+            )
+        else:
+            contract_amounts_str = (
+                f"must be one of: {', '.join(str(amount) for amount in contract_amounts[:-1])}, "
+                f"or {contract_amounts[-1]}."
+            )
+        errors.append(ValidationError("amount", contract_amounts_str))
+    return errors
+
+
+async def participate_as_player(discord_id: int, first_name: str, last_name: str):
+    participant = Participant(
+        discord_id=discord_id,
+        first_name=first_name,
+        last_name=last_name,
+        is_coach=False,
+    )
+    if len(errors := validate_participant(participant)) > 0:
+        raise ValidationException(errors)
+    _db.insert_participant(participant)
+
+
+async def participate_as_coach(discord_id: int, first_name: str, last_name: str):
+    participant = Participant(
+        discord_id=discord_id,
+        first_name=first_name,
+        last_name=last_name,
+        is_coach=True,
+    )
+    if len(errors := validate_participant(participant)) > 0:
+        raise ValidationException(errors)
+    _db.insert_participant(participant)
+
+
+async def end_participation(discord_id: int):
+    if len(errors := validate_discord_id(discord_id)) > 0:
+        raise ValidationException(errors)
+    _db.delete_participant(discord_id)
+    _db.delete_contract(discord_id)
+
+
+async def create_contract(input: ContractInput):
+    participant = Participant(
+        discord_id=input.discord_id,
+        first_name=input.first_name,
+        last_name=input.last_name,
+        is_coach=False,
+    )
+    contract = Contract(discord_id=input.discord_id, amount=input.amount)
+
+    validation_errors = validate_participant(participant) + validate_contract(
+        contract, input.contract_amounts
+    )
+    if len(validation_errors) > 0:
+        raise ValidationException(validation_errors)
+
+    _db.insert_contract(contract)
+
+
+async def end_contract(discord_id: int):
+    if len(errors := validate_discord_id(discord_id)) > 0:
+        raise ValidationException(errors)
+    _db.delete_contract(discord_id)
+
+
+def score_players_and_honorable_mentions(
+    fc_members: list[FCMember],
+    participants: list[Participant],
+    gc_rankings: list[GrandCompanyRanking],
+) -> tuple[list[PlayerScore], list[HonorableMention]]:
+    player_scores = []
+    honorable_mentions = []
+
+    id_to_ranking = {gcr.character_id: gcr for gcr in gc_rankings}
+    for member in fc_members:
+        ranking = id_to_ranking.get(member.ffxiv_id)
+        if ranking is None:
+            continue
+
+        first_name, last_name = member.name.split(" ")
+
+        participant = next(
+            (
+                p
+                for p in participants
+                if p.first_name == first_name and p.last_name == last_name
+            ),
+            None,
+        )
+        if participant is not None:
+            player_scores.append(
+                PlayerScore(
+                    discord_id=participant.discord_id,
+                    first_name=participant.first_name,
+                    last_name=participant.last_name,
+                    seals_earned=ranking.seals,
+                    is_coach=participant.is_coach,
+                )
+            )
+        else:
+            honorable_mentions.append(
+                HonorableMention(
+                    first_name=member.name.split(" ")[0],
+                    last_name=" ".join(member.name.split(" ")[1:]),
+                    rank=ranking.rank,
+                    seals_earned=ranking.seals,
+                )
+            )
+
+    return player_scores, honorable_mentions
+
+
+def find_competition_winner(
+    players: list[PlayerScore],
+) -> tuple[PlayerScore | None, WinReason]:
+    if len(players) == 0:
+        return None, WinReason.NO_ELIGIBLE_PLAYERS
+
+    winner_score = max(player.seals_earned for player in players)
+    winners = [player for player in players if player.seals_earned == winner_score]
+
+    if len(winners) > 1:
+        choice = random.randint(0, len(winners) - 1)
+        return winners[choice], WinReason.TIE_BREAKER
+
+    return winners[0], WinReason.HIGHEST_SEALS
+
+
+def choose_random_drawing_winner(participants: list[PlayerScore]) -> PlayerScore | None:
+    return (
+        None
+        if len(participants) == 0
+        else participants[random.randint(0, len(participants) - 1)]
+    )
+
+
+async def get_competition_results() -> CompetitionResults:
+    participants = _db.get_all_participants()
+    fc_members = _lodestone.get_free_company_members(FREE_COMPANY_ID)
+    gc_rankings = _lodestone.get_grand_company_rankings(WORLD)
+    players, honorable_mentions = score_players_and_honorable_mentions(
+        fc_members, participants, gc_rankings
+    )
+
+    eligible_players = [p for p in players if not p.is_coach]
+    competition_winner, competition_win_reason = find_competition_winner(eligible_players)
+
+    eligible_for_drawing = [
+        p for p in eligible_players if p.discord_id != competition_winner.discord_id
+    ]
+    drawing_winner = choose_random_drawing_winner(eligible_for_drawing)
+    drawing_win_reason = (
+        WinReason.RANDOM_DRAWING
+        if drawing_winner is not None
+        else WinReason.NO_ELIGIBLE_PLAYERS
+    )
+
+    return CompetitionResults(
+        player_scores=players,
+        competition_winner=competition_winner,
+        drawing_winner=drawing_winner,
+        competition_win_reason=competition_win_reason,
+        drawing_win_reason=drawing_win_reason,
+        completed_contracts=[],
+        honorable_mentions=honorable_mentions,
+    )
---
+++ test/test_professionals.py
@@ -0,0 +1,388 @@
+from test.request_mocking import mock_fc_members_response, mock_gc_rankings_response
+import unittest
+
+import responses
+
+from domain import GrandCompanyRanking, HonorableMention, WinReason
+from professionals import *
+
+tc = unittest.TestCase()
+contract_values = [300000, 420000, 500000, 800000, 1000000]
+default_discord_id = 123456789012345678
+default_name = "Juhdu Khigbaa"
+default_first_name = "Juhdu"
+default_last_name = "Khigbaa"
+
+
+def default_contract(
+    discord_id=default_discord_id,
+    amount=500000,
+) -> Contract:
+    return Contract(
+        discord_id=discord_id,
+        amount=amount,
+    )
+
+
+def default_participant(
+    discord_id=default_discord_id,
+    first_name=default_first_name,
+    last_name=default_last_name,
+    is_coach=False,
+) -> Participant:
+    return Participant(
+        discord_id=discord_id,
+        first_name=first_name,
+        last_name=last_name,
+        is_coach=is_coach,
+    )
+
+
+def default_contract_input(
+    discord_id=default_discord_id,
+    first_name=default_first_name,
+    last_name=default_last_name,
+    amount=500000,
+) -> ContractInput:
+    return ContractInput(
+        discord_id=discord_id,
+        first_name=first_name,
+        last_name=last_name,
+        amount=amount,
+        contract_amounts=contract_values,
+    )
+
+
+def default_player_score(
+    discord_id=default_discord_id,
+    first_name=default_first_name,
+    last_name=default_last_name,
+    seals_earned=500000,
+) -> PlayerScore:
+    return PlayerScore(
+        discord_id=discord_id,
+        first_name=first_name,
+        last_name=last_name,
+        seals_earned=seals_earned,
+        is_coach=False,
+    )
+
+
+def assert_error(errors, field, message):
+    tc.assertEqual(len(errors), 1)
+    tc.assertEqual(errors[0].field, field)
+    tc.assertEqual(errors[0].message, message)
+
+
+class TestValidateParticipant(unittest.TestCase):
+    def test_valid_input(self):
+        errors = validate_participant(default_participant())
+        self.assertEqual(len(errors), 0)
+
+    def test_invalid_discord_id(self):
+        tests = ["not_an_int", 123.456, None, [], {}]
+        for test in tests:
+            with self.subTest(discord_id=test):
+                errors = validate_participant(default_participant(discord_id=test))
+                assert_error(errors, "discord_id", "must be an integer.")
+
+    def test_invalid_first_name(self):
+        tests = ["", "Juhdu with Spaces", "Juhdu-Khigbaa", " ", "/4iieh)OEWP\\"]
+        for test in tests:
+            with self.subTest(first_name=test):
+                errors = validate_participant(default_participant(first_name=test))
+                assert_error(errors, "first_name", "must be non-empty and alphabetic.")
+
+    def test_invalid_last_name(self):
+        tests = ["", "Khigbaa with Spaces", "Khigbaa-Khigbaa", " ", "/4iieh)OEWP\\"]
+        for test in tests:
+            with self.subTest(last_name=test):
+                errors = validate_participant(default_participant(last_name=test))
+                assert_error(errors, "last_name", "must be non-empty and alphabetic.")
+
+
+class TestValidateContract(unittest.TestCase):
+    def test_valid_input(self):
+        errors = validate_contract(default_contract(), contract_values)
+        self.assertEqual(len(errors), 0)
+
+    def test_invalid_discord_id(self):
+        tests = ["not_an_int", 123.456, None, [], {}]
+        for test in tests:
+            with self.subTest(discord_id=test):
+                errors = validate_contract(
+                    default_contract(discord_id=test), contract_values
+                )
+                assert_error(errors, "discord_id", "must be an integer.")
+
+    def test_invalid_seals(self):
+        tests = [0, -1, -100, 300001, 430000, 1e8]
+        for test in tests:
+            with self.subTest(amount=test):
+                errors = validate_contract(default_contract(amount=test), contract_values)
+                assert_error(
+                    errors,
+                    "amount",
+                    "must be one of: 300000, 420000, 500000, 800000, or 1000000.",
+                )
+
+
+class TestParticipation(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.db = SqlLiteClient(":memory:")
+        initialize(self.db, None)
+
+    async def test_should_store_valid_participant_as_non_coach(self):
+        participant = default_participant()
+        await participate_as_player(
+            participant.discord_id, participant.first_name, participant.last_name
+        )
+        stored_participant = self.db.get_participant(participant.discord_id)
+        self.assertEqual(stored_participant, participant)
+
+    async def test_should_store_valid_coach(self):
+        coach = default_participant(is_coach=True)
+        await participate_as_coach(coach.discord_id, coach.first_name, coach.last_name)
+        stored_participant = self.db.get_participant(coach.discord_id)
+        self.assertEqual(stored_participant, coach)
+
+    async def test_invalid_participant(self):
+        with self.assertRaises(ValidationException) as ve:
+            await participate_as_player("not an int", "Juhdu 123", "Kh igs09j3kE$$##baa")
+        errors = ve.exception.errors
+        self.assertEqual(len(errors), 3)
+
+    async def test_invalid_coach(self):
+        with self.assertRaises(ValidationException) as ve:
+            await participate_as_coach("not an int", "", "Khigbaa123")
+        errors = ve.exception.errors
+        self.assertEqual(len(errors), 3)
+
+    async def test_should_end_participation(self):
+        participant = default_participant()
+        await participate_as_player(
+            participant.discord_id, participant.first_name, participant.last_name
+        )
+        await end_participation(participant.discord_id)
+        stored_participant = self.db.get_participant(participant.discord_id)
+        self.assertIsNone(stored_participant)
+
+    async def test_should_end_participation_with_contract(self):
+        participant = default_participant()
+        contract = default_contract()
+        await participate_as_player(
+            participant.discord_id, participant.first_name, participant.last_name
+        )
+        await create_contract(default_contract_input())
+        await end_participation(participant.discord_id)
+        stored_participant = self.db.get_participant(participant.discord_id)
+        stored_contract = self.db.get_contract(contract.discord_id)
+        self.assertIsNone(stored_participant)
+        self.assertIsNone(stored_contract)
+
+
+class TestCreateContract(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.db = SqlLiteClient(":memory:")
+        initialize(self.db, None)
+
+    async def test_valid_contract_creation(self):
+        await create_contract(default_contract_input())
+        stored_contract = self.db.get_contract(default_discord_id)
+        self.assertEqual(stored_contract, default_contract())
+
+    async def test_invalid_contract_raises_exception(self):
+        input = default_contract_input(amount=-500000, first_name="Juhdu 123")
+        with self.assertRaises(ValidationException) as ve:
+            await create_contract(input)
+        errors = ve.exception.errors
+        self.assertEqual(len(errors), 2)
+
+    async def test_should_end_contract(self):
+        contract = default_contract()
+        await create_contract(default_contract_input())
+        await end_contract(contract.discord_id)
+        stored_contract = self.db.get_contract(contract.discord_id)
+        self.assertIsNone(stored_contract)
+
+
+class TestFindCompetitionWinner(unittest.TestCase):
+    def test_no_players(self):
+        winner, reason = find_competition_winner([])
+        self.assertIsNone(winner)
+        self.assertEqual(reason, WinReason.NO_ELIGIBLE_PLAYERS)
+
+    def test_single_player(self):
+        player = default_player_score()
+        winner, reason = find_competition_winner([player])
+        self.assertEqual(winner, player)
+        self.assertEqual(reason, WinReason.HIGHEST_SEALS)
+
+    def test_multiple_players_highest_seals(self):
+        player1 = default_player_score()
+        player2 = PlayerScore(456, "Another", "Player", 800000)
+        player3 = PlayerScore(789, "Third", "Player", 300000)
+        winner, reason = find_competition_winner([player1, player2, player3])
+        self.assertEqual(winner, player2)
+        self.assertEqual(reason, WinReason.HIGHEST_SEALS)
+
+    def test_tie_breaker(self):
+        player1 = default_player_score()
+        player2 = PlayerScore(456, "Another", "Player", 500000)
+        player3 = PlayerScore(789, "Third", "Gamer", 300000)
+        winner, reason = find_competition_winner([player1, player2, player3])
+        self.assertIsNotNone(winner)
+        self.assertIn(winner, [player1, player2])
+        self.assertEqual(reason, WinReason.TIE_BREAKER)
+
+
+class TestFindDrawingWinner(unittest.TestCase):
+    def test_no_players(self):
+        winner = choose_random_drawing_winner([])
+        self.assertIsNone(winner)
+
+    def test_single_player(self):
+        player = default_player_score()
+        winner = choose_random_drawing_winner([player])
+        self.assertEqual(winner, player)
+
+    def test_multiple_players(self):
+        player1 = default_player_score()
+        player2 = default_player_score(456, "Another", "Player", 600000)
+        player3 = default_player_score(789, "Third", "Player", 300000)
+        winner = choose_random_drawing_winner([player1, player2, player3])
+        self.assertIn(winner, [player1, player2, player3])
+
+
+class TestGetCompetitionResults(unittest.IsolatedAsyncioTestCase):
+    HOSTNAME = "fake.lodestone.test"
+    BASE_URL = f"https://{HOSTNAME}"
+
+    def setUp(self):
+        self.db = SqlLiteClient(":memory:")
+        self.lodestone = LodestoneScraper(self.BASE_URL)
+        initialize(self.db, self.lodestone)
+
+    def setup_players(self, ffxiv_ids_to_players, ffxiv_ids_to_honorable_mentions={}):
+        for _, player in ffxiv_ids_to_players.items():
+            self.db.insert_participant(
+                Participant(
+                    discord_id=player.discord_id,
+                    first_name=player.first_name,
+                    last_name=player.last_name,
+                    is_coach=False,
+                )
+            )
+
+        fc_members = [
+            FCMember(
+                ffxiv_id=id,
+                name=f"{player.first_name} {player.last_name}",
+                rank="Member",
+            )
+            for id, player in ffxiv_ids_to_players.items()
+        ] + [
+            FCMember(
+                ffxiv_id=id,
+                name=f"{hm.first_name} {hm.last_name}",
+                rank="Member",
+            )
+            for id, hm in ffxiv_ids_to_honorable_mentions.items()
+        ]
+
+        responses.add(
+            mock_fc_members_response(
+                self.HOSTNAME, 200, FREE_COMPANY_ID, members=fc_members
+            )
+        )
+
+        gc_rankings = [
+            GrandCompanyRanking(
+                character_id=id,
+                character_name=f"{player.first_name} {player.last_name}",
+                rank=1,
+                seals=player.seals_earned,
+            )
+            for id, player in ffxiv_ids_to_players.items()
+        ] + [
+            GrandCompanyRanking(
+                character_id=id,
+                character_name=f"{hm.first_name} {hm.last_name}",
+                rank=hm.rank,
+                seals=hm.seals_earned,
+            )
+            for id, hm in ffxiv_ids_to_honorable_mentions.items()
+        ]
+
+        gc_responses = [
+            mock_gc_rankings_response(self.HOSTNAME, 200, "Siren", gc_rankings, 1),
+            mock_gc_rankings_response(self.HOSTNAME, 200, "Siren", [], 2),
+            mock_gc_rankings_response(self.HOSTNAME, 200, "Siren", [], 3),
+            mock_gc_rankings_response(self.HOSTNAME, 200, "Siren", [], 4),
+            mock_gc_rankings_response(self.HOSTNAME, 200, "Siren", [], 5),
+        ]
+
+        for gc_response in gc_responses:
+            responses.add(gc_response)
+
+    @responses.activate
+    async def test_should_return_no_results_when_no_competitors(self):
+        self.setup_players({})
+        results = await get_competition_results()
+        self.assertEqual(results.player_scores, [])
+        self.assertIsNone(results.competition_winner)
+        self.assertEqual(results.competition_win_reason, WinReason.NO_ELIGIBLE_PLAYERS)
+        self.assertIsNone(results.drawing_winner)
+        self.assertEqual(results.drawing_win_reason, WinReason.NO_ELIGIBLE_PLAYERS)
+        self.assertEqual(results.completed_contracts, [])
+
+    @responses.activate
+    async def test_should_return_single_competitor_as_winner(self):
+        player = default_player_score()
+        self.setup_players({"123": player})
+
+        results = await get_competition_results()
+        self.assertIn(player, results.player_scores)
+        self.assertEqual(results.competition_winner, player)
+        self.assertEqual(results.competition_win_reason, WinReason.HIGHEST_SEALS)
+        self.assertIsNone(results.drawing_winner)
+        self.assertEqual(results.drawing_win_reason, WinReason.NO_ELIGIBLE_PLAYERS)
+        self.assertEqual(results.completed_contracts, [])
+
+    @responses.activate
+    async def test_should_assign_two_competitors_correctly(self):
+        player = default_player_score()
+        player2 = default_player_score(
+            discord_id=987654321098765432,
+            first_name="Another",
+            last_name="Player",
+            seals_earned=300000,
+        )
+        self.setup_players({"123": player, "456": player2})
+
+        results = await get_competition_results()
+        self.assertIn(player, results.player_scores)
+        self.assertIn(player2, results.player_scores)
+        self.assertEqual(results.competition_winner, player)
+        self.assertEqual(results.competition_win_reason, WinReason.HIGHEST_SEALS)
+        self.assertEqual(results.drawing_winner, player2)
+        self.assertEqual(results.drawing_win_reason, WinReason.RANDOM_DRAWING)
+
+    @responses.activate
+    async def test_should_award_honorable_mentions(self):
+        player = default_player_score()
+        honorable_mention = HonorableMention(
+            first_name="Honorable",
+            last_name="Mention",
+            rank=10,
+            seals_earned=250000,
+        )
+        self.setup_players({"123": player}, {"honorable1": honorable_mention})
+
+        results = await get_competition_results()
+        self.assertIn(player, results.player_scores)
+        self.assertEqual(results.competition_winner, player)
+        self.assertEqual(results.competition_win_reason, WinReason.HIGHEST_SEALS)
+        self.assertIsNone(results.drawing_winner)
+        self.assertEqual(results.drawing_win_reason, WinReason.NO_ELIGIBLE_PLAYERS)
+        self.assertEqual(results.honorable_mentions, [honorable_mention])
