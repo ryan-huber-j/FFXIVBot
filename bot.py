f75a848d559ab6d17bd006399f99dbe36d5f9f1f
Ryan Huber <hryan13x@gmail.com>
2025-10-29 20:43:22 -0700

    Finished up get competition results command

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import string
 import textwrap
-from typing import NamedTuple
+from typing import Iterable, NamedTuple
 
 from bs4 import BeautifulSoup
 import discord
@@ -12,44 +12,59 @@ import requests
 from db import SqlLiteClient
 from domain import (
     CompetitionResults,
+    Contract,
     ContractInput,
     HonorableMention,
     Participant,
     PlayerScore,
     UserException,
     ValidationException,
+    WinReason,
 )
 from lodestone import LodestoneScraper
 import professionals
 
 DUPLICATION_EXPLANATION = " *Note: Defaulted to highest rank listed and combined score between two ranks earned*"
 WINNER_MESSAGE = " WINNER"
-RESULTS_MESSAGE_TEMPLATE = """
+
+PARTICIPANT_MESSAGE_TEMPLATE = """
 ✅ **Participants**
 Note: ranks Labeled "???" were less than the server-wide top 500 or unlisted at all.
-{formatted_participants}
+{}
+""".strip()
+COACHES_MESSAGE_TEMPLATE = "🛂 **Coaches**\n{}"
 
-🛂 **Coaches**
-{formatted_coaches}
-
+HONORABLE_MENTIONS_MESSAGE_TEMPLATE = """
 ⚠ **Honorable Mentions**
 These were people who were non-participants but made it to top 500 and were in our FC!
-{honorable_mentions_list}
+{}
+""".strip()
 
+WINNER_MESSAGE_TEMPLATE = """
 **Winner:** {mention_winner}
 
 For winning, {mention_winner} gets to choose from:
 Any Mog Station Items equaling $10.00 USD (before tax; some Square Enix implemented limitations apply).
 or
 $10.00 Amazon Gift Card
+"""
 
+CONTRACTS_MESSAGE_TEMPLATE = """
 📜 **Contracts this Week**
-{contracts_list}
-
-Special thanks to {mention_author} for maintaining our Discord bot {mention_bot_user} to help with rank info collection!
-
-Winner of the Random Prize - Gil Drawing was awarded {mention_drawing_winner}! An extra 350,000 Gil was sent to them! Remember: This prize is awarded to folks who didn't win the Mog Station prizes this week but still scored in the top 500 as a participant! Please make sure to sign up if you plan to participate to ensure you can be counted. See the Honorable Mentions list to see if YOU made the top 500 this week.
+{}
 """.strip()
+
+CREDITS_MESSAGE_TEMPLATE = "Special thanks to {} for maintaining our Discord bot {} to help with rank info collection!"
+
+DRAWING_MESSAGE_TEMPLATE = "Winner of the Random Prize - Gil Drawing was awarded {}! An extra 350,000 Gil was sent to them! Remember: This prize is awarded to folks who didn't win the Mog Station prizes this week but still scored in the top 500 as a participant! Please make sure to sign up if you plan to participate to ensure you can be counted. See the Honorable Mentions list to see if YOU made the top 500 this week."
+
+contract_payouts = {
+    300000: 450000,
+    420000: 550000,
+    500000: 650000,
+    800000: 900000,
+    1000000: 3200000,
+}
 
 
 load_dotenv()
@@ -60,6 +75,18 @@ intents.message_content = True
 client = discord.Client(application_id=os.getenv("APPLICATION_ID"), intents=intents)
 tree = app_commands.CommandTree(client)
 guild = discord.Object(id=int(os.getenv("GUILD_ID")))
+
+
+def mention(user_id):
+    return f"<@{user_id}>"
+
+
+def bold(text):
+    return f"**{text}**"
+
+
+def italicize(text):
+    return f"*{text}*"
 
 
 def run_bot(discord_token):
@@ -224,14 +251,8 @@ async def end_contract(interaction: discord.Interaction):
     await interaction.followup.send(msg)
 
 
-def get_fc_member_ids() -> list[str]:
-    scraper = LodestoneScraper("https://na.finalfantasyxiv.com")
-    fc_members = scraper.get_free_company_members("9231394073691073564")
-    return [member.ffxiv_id for member in fc_members]
-
-
 async def consume_results(interaction: discord.Interaction) -> CompetitionResults:
-    async for result in professionals.get_competition_results():
+    async for result in professionals.get_competition_results(contract_payouts):
         if isinstance(result, str):
             await interaction.followup.send(result)
         elif isinstance(result, CompetitionResults):
@@ -244,9 +265,51 @@ def format_participant_list(players: list[PlayerScore | HonorableMention]) -> st
     lines = []
     for p in players:
         line = f"Rank {p.rank}: {p.first_name} {p.last_name} - **{p.seals_earned:,}**"
-        if p.is_winner:
-            line += " WINNER"
         lines.append(line)
+    return "\n".join(lines)
+
+
+def format_participants_msg(players: list[Participant]) -> str:
+    if len(players) == 0:
+        participant_msg = italicize("No participants this week.")
+    else:
+        participant_msg = format_participant_list(players)
+    return PARTICIPANT_MESSAGE_TEMPLATE.format(participant_msg)
+
+
+def format_coach_msg(coaches: list[Participant]) -> str:
+    if len(coaches) == 0:
+        coach_msg = italicize("No coaches this week.")
+    else:
+        coach_msg = format_participant_list(coaches)
+    return COACHES_MESSAGE_TEMPLATE.format(coach_msg)
+
+
+def format_honorable_mentions_msg(mentions: list[HonorableMention]) -> str:
+    if len(mentions) == 0:
+        return italicize("No honorable mentions this week.")
+    return HONORABLE_MENTIONS_MESSAGE_TEMPLATE.format(format_participant_list(mentions))
+
+
+def format_contracts(contracts: list[Contract]) -> str:
+    if len(contracts) == 0:
+        return italicize("No contracts this week.")
+
+    lines = []
+    for cr in contracts:
+        if cr.is_completed:
+            contract_line = (
+                f"{cr.first_name} {cr.last_name}: {cr.amount:,} seals"
+                f" -- Payout: {cr.payout:,} gil"
+            )
+        else:
+            contract_line = (
+                "~~"
+                f"~~{cr.first_name} {cr.last_name}: {cr.amount:,} seals"
+                " -- Contract Incomplete"
+                "~~"
+            )
+        lines.append(contract_line)
     return "\n".join(lines)
 
 
@@ -255,244 +318,47 @@ def format_participant_list(players: list[PlayerScore | HonorableMention]) -> st
     description="Retrieves all FC members' weekly GC ranking results",
     guild=guild,
 )
-async def get_results(interaction: discord.Interaction):
+async def post_competition_results(interaction: discord.Interaction):
     await interaction.response.defer(ephemeral=True, thinking=True)
 
-    results = await invoke_with_exception_handling(
-        interaction, lambda: consume_results(interaction)
+    results: CompetitionResults = await invoke_with_exception_handling(
+        interaction, consume_results, interaction
     )
 
-    participant_msg = format_participant_list(
-        filter(lambda p: not p.is_coach, results.player_scores)
-    )
-    coaches_msg = format_participant_list(
-        filter(lambda p: p.is_coach, results.player_scores)
-    )
-    honorable_mentions_msg = format_participant_list(results.honorable_mentions)
+    participant_scores = [p for p in results.player_scores if not p.is_coach]
+    participant_msg = format_participants_msg(participant_scores)
+    coach_scores = [p for p in results.player_scores if p.is_coach]
+    coaches_msg = format_coach_msg(coach_scores)
+    honorable_mentions_msg = format_honorable_mentions_msg(results.honorable_mentions)
 
-    msg = RESULTS_MESSAGE_TEMPLATE.format(
-        formatted_participants=participant_msg,
-        formatted_coaches=coaches_msg,
-        honorable_mentions_list=honorable_mentions_msg,
-        mention_winner=mention(results.competition_winner.discord_id),
-        contracts_list="(omitted for brevity)",
-        mention_author=mention(interaction.user.id),
-        mention_bot_user=mention(client.user.id),
-        mention_drawing_winner="(omitted for brevity)",
-    )
-
-
-async def old_get_results(interaction: discord.Interaction):
-    await interaction.response.defer(ephemeral=True, thinking=True)
-    channel = discord.utils.get(interaction.guild.channels, name="professionals-signups")
-    if channel is None:
-        await interaction.followup.send("no channel named *professionals-signups* exists")
-        return
-    role = discord.utils.get(interaction.guild.roles, name="Professional")
-    if role is None:
-        await interaction.followup.send(
-            "no role named *Professional* in channel named *professionals-signups*"
-        )
-        return
-    messages = [
-        message async for message in channel.history(limit=100, oldest_first=False)
-    ]
-    message = None
-    for m in messages:
-        if role.id in m.raw_role_mentions:
-            message = m
-            break
-    if message is None:
-        await interaction.followup.send(
-            "no role named *Professional* mentioned in channel named *professionals-signups*"
-        )
-        return
-    fcm = set(get_fc_member_ids())
-    participants = {}
-    coaches = {}
-    for reaction in message.reactions:
-        async for user in reaction.users():
-            if reaction.emoji == "🇵":
-                participants[
-                    str(interaction.guild.get_member(user.id).display_name).lstrip(
-                        string.punctuation + string.whitespace
-                    )
-                ] = "false"
-            if reaction.emoji == "🇨":
-                coaches[
-                    str(interaction.guild.get_member(user.id).display_name).lstrip(
-                        string.punctuation + string.whitespace
-                    )
-                ] = "false"
-    results = {}
-    msg = None
-    for x in range(1, 6):
-        await getResultsOnPage(results, x, fcm, participants, coaches)
-        if msg:
-            await msg.delete()
-        msg = await interaction.followup.send(f"{20 * x}% complete")
-    if msg:
-        await msg.delete()
-
-    winner = mark_winner(results)
-
-    if len(results) == 0:
-        await interaction.followup.send(
-            "Unfortunately I could not find any participants who qualified to win."
-        )
-        return
-
-    participant_list = "\n".join(
-        [
-            "\n".join(
-                build_score_message(user)
-                for user in results.values()
-                if user.designation == "p"
-            ),
-            "\n".join(
-                f"Rank ???: {user} - **???**"
-                for user in participants
-                if participants[user] == "false" and user not in coaches
-            ),
-        ]
-    )
-    coach_list = "\n".join(
-        [
-            "\n".join(
-                build_score_message(user)
-                for user in results.values()
-                if user.designation == "c"
-            ),
-            "\n".join(
-                f"Rank ???: *{user}* - **???**"
-                for user in coaches
-                if coaches[user] == "false"
-            ),
-        ]
-    )
-    honorable_mentions_list = "\n".join(
-        build_score_message(user) for user in results.values() if user.designation == "x"
-    )
-
-    message_parts = [
-        "✅ Participants\nNote: "
-        "Italicized means that the participant was a coach, not competing for contest prizes."
-        " Ranks Labeled '???' were less than the server-wide top 500 or unlisted at all.\n",
-        participant_list.strip(),
-        "\n🛂 Coaches",
-        coach_list.strip(),
-        "\n⚠ Honorable Mentions\n"
-        "These were people who were non-participants but made it to top 500 and were in our FC!",
-        honorable_mentions_list.strip(),
-    ]
-
-    if winner is not None:
-        try:
-            winner_discord_id = next(
-                filter(
-                    lambda member: member.nick == winner.name, interaction.guild.members
-                )
-            ).id
-        except StopIteration:
-            await interaction.followup.send(
-                f"A winner, {winner.name} was found, but I was unable to find their name in Discord to mention them. Please verify that their nickname matches their character name."
-            )
-            return
-
-        message_parts.append(
-            f"\nFor winning, {mention(winner_discord_id)} gets to choose from:\n"
-            "Any Mog Station Items equaling $10.00 USD (before tax; some Square Enix implemented limitations apply).\n"
-            "or\n"
-            "$10.00 Amazon Gift Card"
-        )
-
-    await interaction.followup.send("\n".join(message_parts))
-
-
-def mention(user_id):
-    return f"<@{user_id}>"
-
-
-def mark_winner(results):
-    participants = dict(
-        (id, player) for (id, player) in results.items() if player.designation == "p"
-    )
-    winner_id = max(participants, key=lambda id: participants[id].score, default=None)
-    if winner_id is not None:
-        winner_info = results[winner_id]
-        marked_winner = Scorer(
-            winner_id,
-            winner_info.name,
-            winner_info.score,
-            winner_info.ranking,
-            winner_info.designation,
-            True,
-        )
-        results[winner_id] = marked_winner
-        return marked_winner
+    if results.competition_win_reason == WinReason.NO_ELIGIBLE_PLAYERS:
+        winner_msg = None
     else:
-        return None
+        winner_msg = WINNER_MESSAGE_TEMPLATE.format(
+            mention_winner=mention(results.competition_winner.discord_id),
+        )
 
-
-def build_score_message(player):
-    duplication_explanation = DUPLICATION_EXPLANATION if player.duplicate else ""
-    win_segment = WINNER_MESSAGE if player.winner else ""
-    name_segment = f"*{player.name}*" if player.designation == "c" else player.name
-
-    return f"Rank {player.ranking}: {name_segment} - **{player.score:,}**{win_segment}{duplication_explanation}"
-
-
-async def getResultsOnPage(results, x, fcm, participants, coaches):
-    soup = BeautifulSoup(
-        requests.get(
-            f"https://na.finalfantasyxiv.com/lodestone/ranking/gc/weekly/?page={x}&filter=1"
-            "&worldname=Siren"
-        ).content,
-        "html.parser",
+    contracts_msg = format_contracts(results.contract_results)
+    credits_msg = CREDITS_MESSAGE_TEMPLATE.format(
+        mention(interaction.user.id), mention(client.user.id)
     )
-    ranked_peeps = soup.select("tbody tr")
-    for result in ranked_peeps:
-        player_id = str(result["data-href"]).split("/")[3]
-        if player_id in fcm:
-            score = int(
-                str(result.find("td", {"class": "ranking-character__value"}).text).strip()
-            )
-            name = str(result.find("h4").contents[0]).strip()
-            ranking = int(
-                str(result.select(".ranking-character__number")[0].text).strip()
-            )
-            designation = "x"
-            win = False
-            if name in participants and name not in coaches:
-                designation = "p"
-                participants[name] = "true"
-            if name in coaches:
-                designation = "c"
-                coaches[name] = "true"
-            if player_id in results:
-                results[player_id] = Scorer(
-                    player_id,
-                    name,
-                    score + results.get(player_id).score,
-                    results.get(player_id).ranking,
-                    designation,
-                    win,
-                    True,
-                )
-            else:
-                results[player_id] = Scorer(
-                    player_id, name, score, ranking, designation, win
-                )
 
+    if results.drawing_win_reason == WinReason.NO_ELIGIBLE_PLAYERS:
+        drawing_msg = None
+    else:
+        drawing_msg = DRAWING_MESSAGE_TEMPLATE.format(
+            results.drawing_winner.first_name + " " + results.drawing_winner.last_name,
+        )
 
-class Scorer(NamedTuple):
-    id: int
-    name: str = ""
-    score: int = 0
-    ranking: int = 0
-    designation: str = ""
-    winner: bool = False
-    duplicate: bool = False
+    msg_parts = [
+        participant_msg,
+        coaches_msg,
+        honorable_mentions_msg,
+        winner_msg,
+        contracts_msg,
+        credits_msg,
+        drawing_msg,
+    ]
+    msg = "\n\n".join(part for part in msg_parts if part is not None)
 
-    def __eq__(self, other):
-        return self.name == other.name
+    await interaction.followup.send(msg)
